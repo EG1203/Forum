b55f4bff69f73e5344f681dfa070ee2f6f01c030
maxyaylyan <12.12.2003@inbox.ru>
2024-06-16 22:18:51 +0200

    lasttt
    lasttt

--- Forum/Controllers/CommentsController.cs
+++ Forum/Controllers/CommentsController.cs
@@ -1,8 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using System.Linq;
 using Forum.Models;
 using Forum.Data;
 
@@ -23,14 +22,14 @@ namespace Forum.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Comment>>> GetComments()
         {
-            return await _context.comments.ToListAsync();
+            return await _context.comments.Include(c => c.post).Include(c => c.user).ToListAsync();
         }
 
         // GET: api/Comments/5
         [HttpGet("{id}")]
         public async Task<ActionResult<Comment>> GetComment(int id)
         {
-            var comment = await _context.comments.FindAsync(id);
+            var comment = await _context.comments.Include(c => c.post).Include(c => c.user).FirstOrDefaultAsync(c => c.id == id);
 
             if (comment == null)
             {
@@ -40,44 +39,19 @@ namespace Forum.Controllers
             return comment;
         }
 
-        // PUT: api/Comments/5
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutComment(int id, Comment comment)
-        {
-            if (id != comment.id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(comment).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!CommentExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
         // POST: api/Comments
         [HttpPost]
         public async Task<ActionResult<Comment>> PostComment(Comment comment)
         {
+            if (!_context.posts.Any(p => p.id == comment.post_id) || !_context.users.Any(u => u.id == comment.user_id))
+            {
+                return BadRequest(new { error = "Invalid post_id or user_id" });
+            }
+
             _context.comments.Add(comment);
             await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetComment", new { id = comment.id }, comment);
+            return CreatedAtAction(nameof(GetComment), new { id = comment.id }, comment);
         }
 
         // DELETE: api/Comments/5
@@ -94,11 +68,6 @@ namespace Forum.Controllers
             await _context.SaveChangesAsync();
 
             return NoContent();
-        }
-
-        private bool CommentExists(int id)
-        {
-            return _context.comments.Any(e => e.id == id);
         }
     }
 }
